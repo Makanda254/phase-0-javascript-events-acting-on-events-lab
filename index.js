d368eb312eb120314d60e155c7937e2445ea22c1
Victor John Makanda <victormakanda254@gmail.com>
2023-09-15 21:33:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft(){
+        const leftNumbers = dodger.style.left.replace("px", "");
+        const left = parseInt(leftNumbers, 10);
+      
+        if (left > 0) {
+          dodger.style.left = `${left - 1}px`;
+        }
+}
+
+
+function moveDodgerRight(){
+    const left = parseInt(dodger.style.left);
+    const right = left + 1;
+    if (right + 40 <= 400) { 
+      dodger.style.left = `${right}px`;
+ }
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }else if (e.key === 'ArrowRight') {
+        moveDodgerRight();
+      }
+  });
+
